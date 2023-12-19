39d7fed55ed1e6e2e11d95d7a54f7722b92f0ca4
EmirMaya <emirmayaok@gmail.com>
2023-12-19 12:09:13 -0300

    api image comentado

--- src/app/api/profile/image/route.js
+++ src/app/api/profile/image/route.js
@@ -1,43 +1,43 @@
-import mongoose from 'mongoose';
-import { getServerSession } from 'next-auth';
-import { Buffer } from 'buffer';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { User } from '@/models/User';
+// import mongoose from 'mongoose';
+// import { getServerSession } from 'next-auth';
+// import { Buffer } from 'buffer';
+// import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+// import { User } from '@/models/User';
 
-export const config = {
-    api: {
-        bodyParser: {
-            sizeLimit: '1mb', // Limita el tamaño del cuerpo de la solicitud
-        },
-    },
-};
+// export const config = {
+//     api: {
+//         bodyParser: {
+//             sizeLimit: '1mb', // Limita el tamaño del cuerpo de la solicitud
+//         },
+//     },
+// };
 
-export const POST = async (req, res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    const session = await getServerSession(authOptions);
-    const email = session?.user?.email;
+// export const POST = async (req, res) => {
+//     mongoose.connect(process.env.MONGO_URL);
+//     const session = await getServerSession(authOptions);
+//     const email = session?.user?.email;
 
-    if (!email) {
-        return 'mail unaunthenticated';
-    }
+//     if (!email) {
+//         return 'mail unaunthenticated';
+//     }
 
-    try {
-        // Verificar si req.body es un búfer o una cadena y convertirlo a un búfer
-        const imageBuffer = Buffer.isBuffer(req.body)
-            ? req.body
-            : Buffer.from(req.body.toString(), 'base64');
+//     try {
+//         // Verificar si req.body es un búfer o una cadena y convertirlo a un búfer
+//         const imageBuffer = Buffer.isBuffer(req.body)
+//             ? req.body
+//             : Buffer.from(req.body.toString(), 'base64');
 
-        // Actualizar la imagen en User
-        const result = await User.findOneAndUpdate({ email }, { image: imageBuffer.toString('base64') }, { upsert: true });
+//         // Actualizar la imagen en User
+//         const result = await User.findOneAndUpdate({ email }, { image: imageBuffer.toString('base64') }, { upsert: true });
 
-        if (result) {
-            return Response.json(true);
-        } else {
-            return Response.status(500).json({ error: 'Failed to update image.' });
-        }
+//         if (result) {
+//             return Response.json(true);
+//         } else {
+//             return Response.status(500).json({ error: 'Failed to update image.' });
+//         }
 
-    } catch (error) {
-        console.error(error);
-        return Response.status(500).json({ error: 'Internal server error.' });
-    }
-}
+//     } catch (error) {
+//         console.error(error);
+//         return Response.status(500).json({ error: 'Internal server error.' });
+//     }
+// }
