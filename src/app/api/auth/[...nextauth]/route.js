e53a140a6b0014593645f490b212db8212909a9f
EmirMaya <emirmayaok@gmail.com>
2023-12-20 10:02:37 -0300

    /api/profile/image/route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,21 +1,6 @@
 import { authOptions } from '@/auth/authOptions';
 
-
 import NextAuth from "next-auth";
-
-
-// export async function isAdmin() {
-//     const session = await getServerSession(authOptions);
-//     const userEmail = session?.user?.email;
-//     if (!userEmail) {
-//         return false;
-//     }
-//     const userInfo = await UserInfo.findOne({ email: userEmail });
-//     if (!userInfo) {
-//         return false;
-//     }
-//     return userInfo.admin;
-// }
 
 const handler = NextAuth(authOptions);
 
